0f700f499d1a1286920d2b538ef668a87f90cb6f
Colton B <colton@coltonrb.com>
2022-02-11 10:18:15 -0600

    Add statistic editing, strip trackers

---
+++ assets/stats.js
@@ -0,0 +1,39 @@
+function getInt(id) {return parseInt(document.getElementById(id).value);}
+
+function setStats() {
+
+    stats = {};
+
+    // Store basic stats
+    stats["gamesPlayed"] = getInt("gamesPlayed");
+    // stats["winPercentage"] = getInt("winPercentage");
+    stats["currentStreak"] = getInt("currentStreak");
+    stats["maxStreak"] = getInt("maxStreak");
+
+    // Store guesses
+    guesses = {};
+    guesses["1"] = getInt("guesses_1");
+    guesses["2"] = getInt("guesses_2");
+    guesses["3"] = getInt("guesses_3");
+    guesses["4"] = getInt("guesses_4");
+    guesses["5"] = getInt("guesses_5");
+    guesses["6"] = getInt("guesses_6");
+    
+
+    // Calculate additional attributes
+    gamesWon = Object.values(guesses).reduce((a, b) => a + b);
+    stats["gamesWon"] = gamesWon;
+
+    fail = stats["gamesPlayed"] - gamesWon;
+    guesses["fail"] = fail;
+    stats["guesses"] = guesses;
+
+    winPercentage = Math.round((gamesWon / stats["gamesPlayed"]) * 100);
+    stats["winPercentage"] = winPercentage;
+
+    // Save to local storage
+    myStorage = window.localStorage;
+    myStorage.setItem("statistics", JSON.stringify(stats));
+
+    alert("Stats saved")
+}
